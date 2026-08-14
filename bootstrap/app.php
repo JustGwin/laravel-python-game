a97113d3a11ec1104ef0918e44432bd56cedf22a
Abyse.Eclis <wasmoonk1@gmail.com>
2026-08-14 09:13:42 +0700

    feat: initialize application bootstrap with custom routing, middleware, and exception handling configurations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,10 +6,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: __DIR__ . '/../routes/web.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
